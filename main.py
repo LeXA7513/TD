c04322c0976660ed51ddb13c75c5af3dfe8c520a
LeXA7513 <autogue.axel@gmail.com>
2021-01-19 10:49:57 +0000

    rendu fonctionnelle

--- main.py
+++ main.py
@@ -60,14 +60,20 @@ print('Affichage des points')
 
 class ObjectPoint1D:
   def __init__(self, x):
-    self.x=x
+    self.x = x
+
 def affiche_point1D(point1d):
-  print(point1d.x)
+  print(f"Point1D(x={point1d.x})")
+
+def str_(point1d):
+  return f"Point1D(x={point1d.x})"
 
 np1d = ObjectPoint1D(0.5)
 np1d.x 
-print(np1d)
+affiche_point1D(np1d)
+print(str_(np1d))
 
 np1d1 = ObjectPoint1D(0.6)
 np1d1.x 
-print(np1d1)
+affiche_point1D(np1d1)
+print(str_(np1d1))
